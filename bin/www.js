bb8893762a3fc99fe06086feb4373b6e8fea907b
richardbonneau <richardbonneau@hotmail.com>
2018-09-25 17:23:41 -0400

    playing around with dependencies.

--- bin/www.js
+++ bin/www.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 const http = require('http');
 const app = require('../server');
 
-const port = process.env.SHOPIFY_APP_PORT;
+const port = process.env.PORT;
 app.set('port', port);
 
 const server = http.createServer(app);
