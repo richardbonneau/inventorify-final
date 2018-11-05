81afde0bd370b0bab497be20f9693e8509c67fe7
richardbonneau <richard@pochesetfils.com>
2018-11-05 11:42:42 -0500

    figuring out how to fetch all products

--- client/Main.js
+++ client/Main.js
@@ -67,15 +67,13 @@ export default class Main extends Component {
         var fetches = [];
         for (let i = 0; i < nbPages; i++) {
             fetches.push(
-                new Promise(resolve => setTimeout(resolve, delay)).then(() => {
-
-                    fetch('/shopify/api/products.json?limit=250&page=' + i + 1)
-                        .then(response => response.json())
-                        .then(responseJson => {
-                            console.log(i, "call", responseJson)
-                            arr.push(responseJson)
-                        })
-                })
+                new Promise(resolve => setTimeout(resolve, delay)).then(() =>
+                    fetch('/shopify/api/products.json?limit=250&page=' + i + 1))
+                    .then(response => response.json())
+                    .then(responseJson => {
+                        console.log(i, "call", responseJson)
+                        arr.push(responseJson)
+                    })
             )
             delay += delayIncrement
         }
